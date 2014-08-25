be3a716dfeb8fb2e878472b9ff691774229b0ebe
Tom Crayford <tcrayford@googlemail.com>
2014-08-25 17:18:11 +0100

    include rails startup logging in initializer

--- examples/yeller_example/config/initializers/yeller.rb
+++ examples/yeller_example/config/initializers/yeller.rb
@@ -3,3 +3,9 @@ require 'yeller/rails'
 Yeller::Rails.configure do |config|
   config.token = 'YOUR API TOKEN HERE'
 end
+
+if Yeller::Rails.enabled?
+  Rails.logger.info("cool, yeller is on")
+else
+  Rails.logger.info("yeller ain't on. Likely this is development or test mode")
+end
