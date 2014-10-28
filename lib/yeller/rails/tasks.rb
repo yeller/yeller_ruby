ee12c50543039908d5ba00ac1bd9b7c414969117
Tom Crayford <tcrayford@googlemail.com>
2014-10-28 17:43:43 -0400

    fix verification rake task

--- lib/yeller/rails/tasks.rb
+++ lib/yeller/rails/tasks.rb
@@ -1,39 +1,50 @@
-require "actionpack/lib/action_dispatch/testing/test_request"
-require "actionpack/lib/action_dispatch/testing/test_response"
+require 'action_controller/test_case'
 namespace :yeller do
   desc "verify your yeller gem installation by sending a test exception to yeller's servers"
   task :verify => [:environment] do
     Dir["app/controllers/application*.rb"].each { |file| require(File.expand_path(file)) }
 
+    catcher = Yeller::Rails::ActionControllerCatchingHooks
+
+    if !([ActionController::Base, ActionDispatch::DebugExceptions, ActionDispatch::ShowExceptions, ApplicationController].any? {|x| x.included_modules.include?(catcher) })
+      puts "NO INITIALIZATION"
+      exit 126
+    end
+
     class YellerVerifyException < RuntimeError; end
 
     Yeller::Rails.configure do |config|
       config.development_environments = []
+    end
 
-      class ApplicationController
-        prepend_before_filter :verify_yeller
+    class ApplicationController
+      prepend_before_filter :verify_yeller
 
-        def verify_yeller
-          raise YellerVerifyException.new("verifying that yeller works")
-        end
-
-        def verify
-        end
-
-        def consider_all_requests_local
-          false
-        end
-
-        def local_request?
-          false
-        end
+      def verify_yeller
+        raise YellerVerifyException.new("verifying that yeller works")
       end
 
-      class YellerVerificationController < ApplicationController; end
+      def verify
+      end
 
-      request = ActionController::TestRequest.new("REQUEST_URI" => "/yeller_verification_controller")
-      response = ActionController::TestResponse.new
-      YellerVerificationController.new.process(request, response)
+      def consider_all_requests_local
+        false
+      end
+
+      def local_request?
+        false
+      end
     end
+
+    class YellerVerificationController < ApplicationController; end
+
+    puts "TOKEN: #{Yeller::Rack.instance_variable_get('@client').inspect}"
+
+    Rails.application.routes.draw do
+      root 'yeller_verification#verify'
+    end
+
+    env = Rack::MockRequest.env_for('http://example.com')
+    Rails.application.call(env)
   end
 end
