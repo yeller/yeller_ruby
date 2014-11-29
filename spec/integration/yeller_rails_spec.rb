fd8d199e02132f13b4009b717093e2ceedcf27ed
Tom Crayford <tcrayford@googlemail.com>
2014-11-29 13:50:20 +0000

    setup travis build matrix

--- spec/integration/yeller_rails_spec.rb
+++ spec/integration/yeller_rails_spec.rb
@@ -1,5 +1,43 @@
 rails_version = defined?(Rails.version) && Rails.version || defined?(Rails::VERSION::STRING) && Rails::VERSION::STRING
 if rails_version && rails_version.to_f < 3
+  require File.expand_path('../../../lib/yeller/verify_log', __FILE__)
+  Yeller::VerifyLog.enable!
+  require 'activesupport'
+  require 'action_controller'
+  require 'action_controller/test_case'
+  require File.expand_path('../../../lib/yeller', __FILE__)
+  require File.expand_path('../../../lib/yeller/rails', __FILE__)
+  require File.expand_path('../support/fake_yeller_api', __FILE__)
+
+  describe Yeller::Rails do
+    class CustomException < StandardError; end
+    class FakeController < ActionController::Base
+      def index
+        Yeller::VerifyLog.about_to_raise_exception_in_controller!
+        raise CustomException.new
+      end
+    end
+
+    it "submits an error" do
+      FakeYellerApi.start('token', 8888) do |yeller_api|
+        Yeller::Rails.configure do |client|
+          client.token = 'token'
+          client.remove_default_servers
+          client.environment = 'production'
+          client.add_insecure_server "localhost", 8888
+        end
+
+        request = ActionController::TestRequest.new("REQUEST_URI" => "/yeller_fake_controller")
+        response = ActionController::TestResponse.new
+        FakeController.new.process(request, response)
+        unless yeller_api.has_received_exception?(CustomException.new)
+          Yeller::StdoutVerifyLog.print_log!
+        end
+        yeller_api.should have_received_exception(CustomException.new)
+      end
+    end
+  end
+else
   require 'rails'
   require 'action_controller'
   require 'action_controller/metal'
@@ -35,44 +73,6 @@ if rails_version && rails_version.to_f < 3
         end
         env = Rack::MockRequest.env_for("http://example.com")
         Rails.application.call(env)
-        yeller_api.should have_received_exception(CustomException.new)
-      end
-    end
-  end
-else
-  require File.expand_path('../../../lib/yeller/verify_log', __FILE__)
-  Yeller::VerifyLog.enable!
-  require 'activesupport'
-  require 'action_controller'
-  require 'action_controller/test_case'
-  require File.expand_path('../../../lib/yeller', __FILE__)
-  require File.expand_path('../../../lib/yeller/rails', __FILE__)
-  require File.expand_path('../support/fake_yeller_api', __FILE__)
-
-  describe Yeller::Rails do
-    class CustomException < StandardError; end
-    class FakeController < ActionController::Base
-      def index
-        Yeller::VerifyLog.about_to_raise_exception_in_controller!
-        raise CustomException.new
-      end
-    end
-
-    it "submits an error" do
-      FakeYellerApi.start('token', 8888) do |yeller_api|
-        Yeller::Rails.configure do |client|
-          client.token = 'token'
-          client.remove_default_servers
-          client.environment = 'production'
-          client.add_insecure_server "localhost", 8888
-        end
-
-        request = ActionController::TestRequest.new("REQUEST_URI" => "/yeller_fake_controller")
-        response = ActionController::TestResponse.new
-        FakeController.new.process(request, response)
-        unless yeller_api.has_received_exception?(CustomException.new)
-          Yeller::StdoutVerifyLog.print_log!
-        end
         yeller_api.should have_received_exception(CustomException.new)
       end
     end
