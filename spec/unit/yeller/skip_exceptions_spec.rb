355cb6b874c6ddf0b3ee1d3d5012b9db16b7e0c0
Tom Crayford <tcrayford@googlemail.com>
2014-11-30 21:34:57 +0000

    fix skip expectations spec

--- spec/unit/yeller/skip_exceptions_spec.rb
+++ spec/unit/yeller/skip_exceptions_spec.rb
@@ -5,21 +5,21 @@ describe Yeller::SkipExceptions do
 
   it "skips an exception in the given array" do
     skipper = Yeller::SkipExceptions.new(['CustomException1'], Proc.new {})
-    skipper.skip?(CustomException1.new).should be_true
+    skipper.skip?(CustomException1.new).should == true
   end
 
   it "doesn't skip exceptions not in the array" do
     skipper = Yeller::SkipExceptions.new(['CustomException1'], Proc.new {})
-    skipper.skip?(StandardError.new).should be_false
+    skipper.skip?(StandardError.new).should == nil
   end
 
   it "skips an exception if the callback returns true" do
     skipper = Yeller::SkipExceptions.new([], Proc.new { true })
-    skipper.skip?(CustomException1.new).should be_true
+    skipper.skip?(CustomException1.new).should == true
   end
 
   it "doesn't skip exceptions if the callback is false" do
     skipper = Yeller::SkipExceptions.new([], Proc.new { false })
-    skipper.skip?(StandardError.new).should be_false
+    skipper.skip?(StandardError.new).should == false
   end
 end
